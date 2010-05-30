3a886eb61aefe62dcd4654d3e4854b70add89afe
Hemant Kumar <gethemant@gmail.com>
2010-05-30 15:48:58 +0530

    Add a readme file and remove some commented code

--- lib/restful_route_version.rb
+++ lib/restful_route_version.rb
@@ -2,9 +2,6 @@ require "restful_route_version/restful_route_version_option_merger"
 require "restful_route_version/restful_route_version_dependencies"
 require "restful_route_version/restful_route_version_route_set"
 
-#ActiveSupport::Dependencies.extend(RestfulRouteVersion::RestfulRouteVersionDependencies)
-#ActiveSupport::Dependencies.send(:include,RestfulRouteVersion::RestfulRouteVersionDependencies)
-
 ActionController::Routing::RouteSet::Mapper.send(:include, RestfulRouteVersion::RestfulRouteVersionRouteSet)
 
 
