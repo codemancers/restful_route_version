af68d1b9d048a3082636512f25b2ac678d36b766
Hemant Kumar <gethemant@gmail.com>
2010-05-30 16:51:27 +0530

    use a module rather than directly extending ActiveSupport::Dependecies

--- lib/restful_route_version/restful_route_version_dependencies.rb
+++ lib/restful_route_version/restful_route_version_dependencies.rb
@@ -1,11 +1,32 @@
-module ActiveSupport
-  module Dependencies
-    alias_method :old_remove_unloadable_constants!, :remove_unloadable_constants!
+# module ActiveSupport
+#   module Dependencies
+#     alias_method :old_remove_unloadable_constants!, :remove_unloadable_constants!
+    
+#     mattr_accessor :dynamically_defined_constants
+#     @@dynamically_defined_constants = Set.new()
+
+#     def remove_unloadable_constants!
+#       autoloaded_constants.each { |const|
+#         remove_constant const unless dynamically_defined_constants.include?(const)
+#       }
+#       autoloaded_constants.clear
+#       explicitly_unloadable_constants.each { |const| remove_constant const }
+#     end
+#   end
+# end
+
+module RestfulRouteVersion
+  module RestfulRouteVersionDependencies
+    def self.included(base)
+      base.send(:remove_method, :remove_unloadable_constants!)
+    end
+    #alias_method :old_remove_unloadable_constants!, :remove_unloadable_constants!
     
     mattr_accessor :dynamically_defined_constants
     @@dynamically_defined_constants = Set.new()
 
     def remove_unloadable_constants!
+      p @@dynamically_defined_constants
       autoloaded_constants.each { |const|
         remove_constant const unless dynamically_defined_constants.include?(const)
       }
