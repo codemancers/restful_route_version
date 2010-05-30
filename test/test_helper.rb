9ac73cd902cc1f39abb909ae921267cce0b38e40
Hemant Kumar <gethemant@gmail.com>
2010-05-31 00:30:03 +0530

    check in new code for rails_sandbox

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,6 +11,12 @@ require "action_pack"
 
 require "shoulda"
 
+class Rails
+  def self.root
+    File.expand_path(File.join(File.dirname(__FILE__),"rails_sandbox"))
+  end
+end
+
 $:<< File.join(File.dirname(__FILE__),"..","lib")
 
 require "restful_route_version"
